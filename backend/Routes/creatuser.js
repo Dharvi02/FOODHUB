aa722e64b15d791aa6b15e9c6d888e3852753db1
Dharvi Chaudhary <74051911+Dharvi02@users.noreply.github.com>
2023-08-20 18:11:45 +0530

    Add files via upload

---
+++ backend/Routes/creatuser.js
@@ -0,0 +1,89 @@
+const express=require('express');
+const router =express.Router();
+const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
+const bcrypt = require("bcryptjs");
+const jwt =require("jsonwebtoken")
+const jwtSecret ="mynameisdharvichaudharyinthree";
+router.post("/creatuser",[
+body('email').isEmail(),
+// password must be at least 5 chars long
+body('name').isLength({ min: 5 }),
+body('password','Incorrect password').isLength({ min: 5 })],
+async(req,res)=>{
+
+
+
+    const errors = validationResult(req);          
+    if (!errors.isEmpty()) {                
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const salt =await bcrypt.genSalt(10);
+    let secPassword = await bcrypt.hash(req.body.password,salt) //bcrypt's function
+
+
+try { 
+   await User.create({
+        name:req.body.name,
+        password:secPassword,
+        email:req.body.email,
+        location:req.body.location
+
+    }).then(res.json({success:true})) ;
+} 
+
+catch (error) {
+    console.log(error);
+    res.json({success:false});
+}
+})
+
+
+
+router.post("/loginuser",[body('email').isEmail(),
+// password must be at least 5 chars long
+
+body('password','Incorrect password').isLength({ min: 5 }),async(req,res)=>{
+  
+    const errors = validationResult(req);          
+    if (!errors.isEmpty()) {                
+      return res.status(400).json({ errors: errors.array() });
+    }
+       let email = req.body.email;
+
+
+
+    try {
+   let userData  = await User.findOne({email});
+       if(!userData){
+        return res.status(400).json({ errors:"try loggin with correct details" });
+       }
+
+      const pwdCompare = await bcrypt.compare(req.body.password,userData.password);
+      
+       if(!pwdCompare){
+        return res.status(400).json({ errors:"try loggin with correct details" });
+       }
+      const data ={
+        user:{
+            id:userData.id
+        }
+      }
+
+
+// const authToken = jwt.sign(data.jwtSecret)
+
+return res.json({success:true}); //,authToken:authToken // when i am showing this authtoken then success= false 
+
+    }   
+    catch (error) {
+        console.log(error);
+      
+        res.json({success:false});
+    }
+  
+
+    }])
+
+module.exports= router;
---
+++ backend/db.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const mongoURI =
+  "mongodb+srv://foodHub:mern123@cluster1.qucfqzg.mongodb.net/foodHub?retryWrites=true&w=majority";
+const mongoDB = async () => {
+  await mongoose.connect(
+    mongoURI,
+    { useNewUrlParser: true },
+    async (err, result) => {
+     if (err) console.log("---".err);
+      else {
+        console.log("connected");
+        const fetched_data = await mongoose.connection.db.collection("food_items");
+        fetched_data.find({}).toArray(async function (err, data) {        
+          const foodCategory =
+           mongoose.connection.db.collection("foodcategory");
+          
+           foodCategory.find({}).toArray(function (err, catData) {
+            if (err) console.log(err);
+            else {
+              global.food_items = data;
+              global.foodCategory = catData;
+            }
+          });
+
+          // }
+    });
+
+
+  }
+    }
+  );
+};
+
+module.exports = mongoDB;
